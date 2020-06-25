a5271f386a7375ce403fb0753dd4b1450986a2da
hbrashid <hbrashid@gmail.com>
2020-06-25 12:36:55 -0500

    Added redux-thunk

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Button,
+  Menu,
+  MenuItem,
+  //   IconButton
+} from "@material-ui/core";
+import { MoreVert } from "@material-ui/icons";
 
 const Import = (props) => {
-    // fill out this component
+  // fill out this component
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const ITEM_HEIGHT = 48;
 
-    return (
-        <p>Import Component</p>
-    )
-}
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-export default Import
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Container maxWidth="lg" className="car-container">
+      <Button
+        variant="contained"
+        className="import"
+        color="primary"
+        onClick={props.fetchMakes}
+      >
+        Import
+      </Button>
+      <h2>COUNT: {props.makes.length}</h2>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((make, idx) => (
+            <TableRow key={idx}>
+              {/* <TableCell component="th" scope="row">
+                            {make.id}
+                        </TableCell> */}
+              <TableCell>{make.MakeId}</TableCell>
+              <TableCell>{make.MakeName}</TableCell>
+              <TableCell>
+                <MoreVert onClick={handleClick} />
+
+                <Menu
+                  id="long-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={open}
+                  onClose={handleClose}
+                  PaperProps={{
+                    style: {
+                      maxHeight: ITEM_HEIGHT * 4.5,
+                      width: "20ch",
+                    },
+                  }}
+                >
+                  <MenuItem onClick={props.deleteMake} index={idx}>
+                    Delete
+                  </MenuItem>
+                </Menu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+};
+
+export default Import;
